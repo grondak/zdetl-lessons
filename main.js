6fd739ca7131a191218da76e529b3495f95d37c3
grondak <grondak@gmail.com>
2021-09-02 19:37:29 -0400

    Initial commit: shows all words for all lessons

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            showWelcome: true,
+            welcomeImage: "assets/images/AZ_138-2021.png",
+            welcomeText: "Welcome to First Thousand Words in Zhondai",
+
+        }
+    }
+})
